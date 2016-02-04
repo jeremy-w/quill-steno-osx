30dcf6c1a287f29efc4b75330b6a2a8e5c2ca424
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-03 22:46:06 -0500

    Initial Commit

---
+++ Quill/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quill
+//
+//  Created by Jeremy on 2016-02-03.
+//  Copyright © 2016 Jeremy W. Sherman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
