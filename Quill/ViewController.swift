f00e9ec21a23a10478916b7bea657112e70e9a58
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-04 21:15:45 -0500

    Fix whitespace and line length nits
    Fixed by removing most template code.

--- Quill/ViewController.swift
+++ Quill/ViewController.swift
@@ -3,19 +3,4 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
